a77c9486df958a3fe64eeaa2ff2e8841e55c5e05
Krishna Avtar <krishnaavtar955@gmail.com>
2025-11-02 12:26:57 +0530

    Added cab and bus page

---
+++ src/app/cabs/page.jsx
@@ -0,0 +1,15 @@
+'use client';
+
+import FloatingSearchCard from '@/components/FloatingSearchCard';
+
+export default function CabsPage() {
+
+  return (
+    <div className="container text-[var(--text-color)] mx-auto px-4 py-8">
+      {/* Floating Search Card */}
+      <div className="absolute top-1/4 left-1/2 -translate-x-1/2 -translate-y-1/2">
+        <FloatingSearchCard />
+      </div>
+    </div>
+  );
+}
